41ead9d4643d7b5389b6deff78edd3718892f019
jghenegar <jaylynhenegar@gmail.com>
2022-11-18 17:21:18 -0500

    page 32

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  #resources :articles, to: "articles#index"
+  resources :articles do
+    resources :comments
+  end
+  #get "/articles:id", to: "articles#show"
   mount Avo::Engine, at: Avo.configuration.root_path
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
