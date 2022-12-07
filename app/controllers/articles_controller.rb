2b678095e557e31f6e4ae1b268e47869c1ec188d
jghenegar <jaylynhenegar@gmail.com>
2022-12-07 11:04:25 -0500

    fresh start

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    http_basic_authenticate_with name: "dhh", password: "secret", except:
+    http_basic_authenticate_with name: "jaylyn", password: "secret", except:
   [:index, :show]
 
   def index
